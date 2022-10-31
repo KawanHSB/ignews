a95c79bbfea24b2315117286b2a29054ca0e0254
KawanHSB <kawanbatista@hotmail.com>
2022-10-31 13:40:49 -0300

    Estrutura da aplicação

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Inicio Ig.news</title>
+      </Head>
+      <h1 >
+        Hello world
+      </h1>
+    </>
+  )
+}
